357ffac450a2bf384875628d156dc270ab478e55
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-04-05 21:18:29 +0300

    added models Task,SubTask,Category

---
+++ tasks/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Task, SubTask, Category
+
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Category)
+from django.contrib import admin
+
+# Register your models here.
