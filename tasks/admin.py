11bb31dc7ac190bbfff21834f50e864a2f0977a2
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-04-15 00:51:21 +0300

    Add SubtaskCreation class for adding subtasks during task creation

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
 
-# admin.site.register(Task)
-# admin.site.register(SubTask)
-# admin.site.register(Category)
-# from django.contrib import admin
+
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+    fields = ('title', 'description', 'status', 'deadline')
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at')
+    list_display = ('short_title', 'status', 'deadline', 'created_at')
     search_fields = ('title', 'description')
     list_filter = ('status', 'categories')
+    inlines = [SubTaskInline]
+
+    def short_title(self, obj):
+        return obj.title if len(obj.title) <= 10 else f'{obj.title[:10]}...'
+    short_title.short_description = 'Title'
 
 
 @admin.register(SubTask)
@@ -20,9 +26,14 @@ class SubTaskAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     list_filter = ('status',)
 
+    actions = ['mark_as_done']
+
+    @admin.action(description="Mark selected subtasks as Done")
+    def mark_as_done(self, request, queryset):
+        queryset.update(status='Done')
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
-# Register your models here.
