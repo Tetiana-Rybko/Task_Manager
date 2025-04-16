f9063d1d382d02194c688b3cb3c91d1306ca56db
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-04-17 00:15:12 +0300

    Added TaskSerializer serializer for Task model.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
-from .views import TaskCreateView,TaskListView,TaskDetailView
+from .views import TaskCreateView,TaskListView,TaskDetailView,home,TaskStatsView
 from django.urls import path
 
 
 urlpatterns = [
+    path('stats/', TaskStatsView.as_view(), name='task-stats'),
+    path('', home, name='home'),
     path('create/', TaskCreateView.as_view(), name='tasks-create'),
     path('list/',TaskListView.as_view(), name='tasks-list'),
     path('<int:pk>/', TaskDetailView.as_view(), name='tasks-detail'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,22 @@
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Привет! Всё работает!")
-
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.utils.timezone import now
+from django.db.models import Count
+
+
+def home(request):
+    return HttpResponse("Привет! Всё работает!")
 
 class TaskCreateView(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-# Create your views here.
+
+
 class TaskDetailView(generics.RetrieveAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
@@ -19,3 +24,15 @@ class TaskDetailView(generics.RetrieveAPIView):
 class TaskListView(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class TaskStatsView(APIView):
+    def get(self, request):
+        total_tasks = Task.objects.count()
+        status_count = Task.objects.values('status').order_by('status').annotate(count=Count('status'))
+        overdue_tasks = Task.objects.filter(deadline__lt=now()).count()
+
+        return Response({
+            'total_tasks': total_tasks,
+            'overdue_tasks': overdue_tasks,
+            'status_count': status_count,
+        })
