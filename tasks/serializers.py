f71ee17dfdcb3e8b1c6037ca4cef073594518161
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-04-18 17:03:30 +0300

    Created serializer for CategoryCreateSerializer, overridden create and update methods to check uniqueness

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Task,SubTask
+from .models import Task,SubTask,Category
+from rest_framework.exceptions import ValidationError
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,23 @@ class SubTaskCreateSerializer(serializers.ModelSerializer):
         model = SubTask
         fields = '__all__'
         read_only_fields = ['created_at']
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def validate_title(self, value):
+        if self.instance:
+            if Category.objects.exclude(id=self.instance.id).filter(title=value).exists():
+                raise ValidationError("Category already exists")
+            else:
+                if Category.objects.filter(title=value).exists():
+                    raise ValidationError("Category already exists")
+            return value
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+        return instance
