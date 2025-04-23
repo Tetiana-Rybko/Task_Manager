7fcd3f8192ae135ff8a6aee7f4b3d6274d4efded
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-04-24 01:49:56 +0300

    Refactor views: replaced generic class-based views with APIView classes

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from django.core.serializers import serialize
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,23 +8,32 @@ from django.http import HttpResponse
 from .models import Task,SubTask
 from .serializers import TaskSerializer,SubTaskCreateSerializer,TaskDetailSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 def home(request):
     return HttpResponse("Привет! Всё работает!")
 
-class TaskCreateView(generics.CreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+class TaskCreateView(APIView):
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailView(APIView):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
 
 
-class TaskDetailView(generics.RetrieveAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-class TaskListView(generics.ListAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+class TaskListView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
 class TaskStatsView(APIView):
     def get(self, request):
@@ -56,3 +65,38 @@ def task_detail_view(request, task_id):
 
     serializer = TaskDetailSerializer(task)
     return Response(serializer.data)
+
+
+class SubTaskListCreateView(APIView):
+    def get(self, request):
+        subtasks = SubTask.objects.all()
+        serializer = SubTaskCreateSerializer(subtasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SubTaskCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class SubTaskDetailUpdateDeleteView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(SubTask, pk=pk)
+
+    def get(self, request, pk):
+        subtask = self.get_object(pk)
+        serializer = SubTaskCreateSerializer(subtask)
+        return Response(serialize.data)
+
+    def put(self, request, pk):
+        subtask = self.get_object(pk)
+        serializer = SubTaskCreateSerializer(subtask, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    def delete(self, request, pk):
+        subtask = self.get_object(pk)
+        subtask.delete()
+        return Response(status=204)
