3a06a75f1645290171bb8fa8d440e0882fa4f757
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-11 23:53:28 +0200

    Add cursor pagination for tasks with a limit of 6 objects

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -135,4 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
+'PAGE_SIZE':6,
 }
