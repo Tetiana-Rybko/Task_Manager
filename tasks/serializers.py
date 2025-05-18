89bc9045d0e5c7e703207604ef45a64bf003d420
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-18 21:10:00 +0200

    Added user registration with password validation and hashing

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Task,SubTask,Category
 from rest_framework.exceptions import ValidationError
 from django.utils import timezone
-
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+import re
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -70,3 +73,45 @@ class TaskCreateSerializer(serializers.ModelSerializer):
         if value < now:
             raise serializers.ValidationError("Deadline must be greater than now")
         return value
+
+
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    re_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 're_password']
+
+    def validate(self, attrs):
+        first_name = attrs.get('first_name')
+        last_name = attrs.get('last_name')
+        password = attrs.get('password')
+        re_password = attrs.pop('re_password', None)
+
+        re_pattern = r'^[a-zA-Z]+$'
+        if first_name and not re.match(re_pattern, first_name):
+            raise serializers.ValidationError({"first_name": "Имя должно содержать только латинские буквы."})
+
+        if last_name and not re.match(re_pattern, last_name):
+            raise serializers.ValidationError({"last_name": "Фамилия должна содержать только латинские буквы."})
+
+        if not password:
+            raise serializers.ValidationError({"password": "Пароль обязателен."})
+        if not re_password:
+            raise serializers.ValidationError({"re_password": "Подтверждение пароля обязательно."})
+        if password != re_password:
+            raise serializers.ValidationError({"re_password": "Пароли не совпадают."})
+
+        validate_password(password)
+
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
